ce412cde632edb782610dd1670b6bd49ea71b4ce
chegior <chegiorca@hotmail.com>
2021-01-04 09:37:36 -0700

    Working MENU 100%

--- Palindrome.Client/Program.cs
+++ Palindrome.Client/Program.cs
@@ -18,15 +18,40 @@ namespace Palindrome.Client
                 switch (opt = Console.ReadLine())
                 {
                     case "1":Console.WriteLine("Lets enter the input....");Option1Menu(PalindromeInst); break;
-                    case "2":Console.WriteLine("2");break;
-                    case "3":Console.WriteLine("3");break;
-                    case "4":Console.WriteLine("4");break;
+                    case "2":Console.WriteLine("2");Option2Menu();break;
+                    case "3":Console.WriteLine("3");Option3Menu();break;
+                    case "4":Console.WriteLine("4");Option4Menu();break;
                     default:System.Console.WriteLine("Sorry I dont have that option for now....");break;
                 }
             }
 
             // 
         }
+
+        private static void Option4Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Thank you for using Palindrome Program .....bye now!!!   :).");
+            System.Console.WriteLine("Type a Key to Continue.....");
+            Console.ReadKey();
+        }
+
+        private static void Option3Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Whats a Palindrome");
+            Console.WriteLine("A word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.");
+            //System.Console.WriteLine("Type a Key to Continue.....");
+            Console.ReadKey();
+        }
+
+        private static void Option2Menu()
+        {
+            Console.WriteLine("Sorry....No TEST for now :( ");
+            // System.Console.WriteLine("Type a Key to Continue.....");
+            Console.ReadKey();
+        }
+
         static void MainMenuUser()
         {
             System.Console.WriteLine("Type a Key to Continue.....");
